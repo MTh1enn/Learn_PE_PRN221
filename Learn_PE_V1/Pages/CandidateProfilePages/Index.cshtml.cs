04ffb95fdb477cab21a170227bc02647763cd4b6
MTh1enn <thientnmse173541@fpt.edu.vn>
2024-11-08 13:37:22 +0700

    a

--- Learn_PE_V1/Pages/CandidateProfilePages/Index.cshtml.cs
+++ Learn_PE_V1/Pages/CandidateProfilePages/Index.cshtml.cs
@@ -20,11 +20,22 @@ namespace Learn_PE_V1.Pages.CandidateProfilePages
         }
 
         public IList<CandidateProfile> CandidateProfile { get;set; } = default!;
+        public int PageSize { get; set; } = 3;
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
 
-        public IActionResult OnGet()
+        public IActionResult OnGet(int? pageIndex)
         {
-            CandidateProfile = _candidateProfileRepo.GetCandidates(); // Set the property
-            return Page(); // Return the page to display the results
+            var allCandidates = _candidateProfileRepo.GetCandidates();
+            CurrentPage = pageIndex ?? 1;
+
+            TotalPages = (int)Math.Ceiling((double)allCandidates.Count / PageSize);
+            CandidateProfile = allCandidates
+
+           .Skip((CurrentPage - 1) * PageSize)
+           .Take(PageSize)
+           .ToList();
+            return Page(); 
         }
     }
 }
